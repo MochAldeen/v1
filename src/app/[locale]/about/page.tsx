d7ee7992bebf4b34c6eea42ee95abc7e7f7e3d6a
MochAldeen <161240862+MochAldeen@users.noreply.github.com>
2025-06-10 18:04:09 +0700

    Update page.tsx

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -47,7 +47,7 @@ export default function Page() {
                 <Card className="px-4 py-4">
                   <div className="flex flex-row gap-4">
                     <Avatar className="w-24 h-24">
-                      <AvatarImage src="https://drive.google.com/file/d/1XXtgp0JOU4SfBagYtOCmnMPQVYZlYEd_/view?usp=sharing"/>
+                      <AvatarImage src="/images/aaaa.jpg"/>
                       <AvatarFallback>aL</AvatarFallback>
                     </Avatar>
 
